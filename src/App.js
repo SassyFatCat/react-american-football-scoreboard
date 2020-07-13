e0ee156baba2bb4312063d3b239ba3eb40a64e4f
Ryan Lee <therealsassyfatcat@outlook.com>
2020-07-13 15:36:28 -0500

    saving

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ const [lionScore, setLionsScore] = useState(0);
             {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
             <div className="home__score">{lionScore}</div>
           </div>
-          <div className="timer">00:03</div>
+          <div className="timer">00:00</div>
           <div className="away">
             <h2 className="away__name">Tigers</h2>
             <div className="away__score">{tigerScore}</div>
